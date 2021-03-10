340691656a55c7ec785ca0a8c75d815e71a1ec7d
lepecoder <lepecoder@qq.com>
2021-03-10 21:39:58 +0800

    .

--- leetcode_cn/63.不同路径-ii.py
+++ leetcode_cn/63.不同路径-ii.py
@@ -62,6 +62,33 @@ from typing import List
 
 
 class Solution:
+
+    dp = []
+
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         # 从左上角到右下角，用动态规划的方法，
-        # @lc code=end
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        global dp
+        dp = [[-1] * n for _ in range(m)]
+        return self.f(obstacleGrid, m - 1, n - 1)
+
+    def f(self, obstacleGrid, i, j):
+        if i < 0 or j < 0:
+            return 0
+        elif dp[i][j] != -1:
+            return dp[i][j]
+        if obstacleGrid[i][j] == 1:
+            dp[i][j] = 0
+            # return 0
+        elif i == 0 and j == 0:
+            dp[i][j] = 1
+            # return 1
+        else:
+            dp[i][j] = self.f(obstacleGrid, i - 1, j) + self.f(obstacleGrid, i, j - 1)
+        return dp[i][j]
+
+
+so = Solution()
+obs = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+print(so.uniquePathsWithObstacles(obs))
