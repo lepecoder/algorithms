a1b4fa2a477787f2491dc10c15c517234d8659d3
lepecoder <lepecoder@qq.com>
2021-03-30 21:56:09 +0800

    .

---
+++ a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //
+    int a = 0;
+    if (a == 0)
+        return 2;
+    else if (a == 1) {
+        return a;
+    } else
+        return 1;
+    return 0;
+}
