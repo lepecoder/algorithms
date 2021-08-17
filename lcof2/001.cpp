159e0f2c2859fc936eb497a83365cbb97a6869a4
lepecoder <lepecoder@qq.com>
2021-08-18 01:57:06 +0800

    lcof2

---
+++ lcof2/001.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int divide(int a, int b) {
+        bool posi = true;
+        if (a < 0) {
+            a = -a;
+            posi = ~posi;
+        }
+        if (b < 0) {
+            b = -b;
+            posi = ~posi;
+        }
+        int ret = calc(a, b);
+        if () }
+};
+int main() {
+    //
+    return 0;
+}
