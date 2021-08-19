8a9ff1c3e1406959dc4406d3580a1fb1a90188a3
lepecoder <lepecoder@qq.com>
2021-08-20 01:38:53 +0800

    .

--- lcof2/001.cpp
+++ lcof2/001.cpp
@@ -4,16 +4,35 @@ class Solution {
   public:
     int divide(int a, int b) {
         bool posi = true;
+        if (a == 0) //分子为0的情况
+            return 0;
+
+        if (a == INT_MIN && b == -1) return INT_MAX;
+        if (a == INT_MIN && b == 1) return INT_MIN;
         if (a < 0) {
             a = -a;
-            posi = ~posi;
+            posi = !posi;
         }
         if (b < 0) {
             b = -b;
-            posi = ~posi;
+            posi = !posi;
         }
-        int ret = calc(a, b);
-        if () }
+        int res = 0;
+        int t = 0;
+        int bb;
+        while (a >= b) {
+            bb = b;
+            t = 1;
+            while (a > (bb << 1)) {
+                //
+                bb <<= 1;
+                t <<= 1;
+            }
+            res += t;
+            a -= bb;
+        }
+        return posi ? res : -res;
+    }
 };
 int main() {
     //
