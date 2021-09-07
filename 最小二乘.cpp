120b801b75c971cf66189fbe7d6d773478ffad6c
lepecoder <lepecoder@qq.com>
2021-09-07 16:32:01 +0800

    .

---
+++ 最小二乘.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int error_fun(theta, x, y)
+
+    int main() {
+    int n = 20;
+    vector<int> x(n, 0);
+    for (int i = 0; i < n; i++) x[i] = i + 1;
+    vector<int> y = {3, 4, 5, 5, 2, 4, 7, 8, 11, 8, 12, 11, 13, 13, 16, 17, 18, 17, 19, 21};
+
+    return 0;
+}
