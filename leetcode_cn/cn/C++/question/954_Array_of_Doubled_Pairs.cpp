a12f0b29c40e57472c725840d0c3af08cd2f46eb
lixiaopeng.0 <lixiaopeng.0@bytedance.com>
2022-04-01 15:56:15 +0800

    .

---
+++ leetcode_cn/cn/C++/question/954_Array_of_Doubled_Pairs.cpp
@@ -0,0 +1,18 @@
+// @algorithm @lc id=991 lang=cpp 
+// @title array-of-doubled-pairs
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "algm/algm.h"
+using namespace std;
+// @test([3,1,3,6])=false
+// @test([2,1,2,6])=false
+// @test([4,-2,2,-4])=true
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& arr) {
+
+    }
+};
