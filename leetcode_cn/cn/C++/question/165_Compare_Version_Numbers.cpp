7b84cb35a6391deea2f22a94030bc12cb4f5a7d3
lepecoder <lepecoder@qq.com>
2022-04-08 13:47:08 +0800

    。

---
+++ leetcode_cn/cn/C++/question/165_Compare_Version_Numbers.cpp
@@ -0,0 +1,18 @@
+// @algorithm @lc id=165 lang=cpp 
+// @title compare-version-numbers
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "algm/algm.h"
+using namespace std;
+// @test("1.01","1.001")=0
+// @test("1.0","1.0.0")=0
+// @test("0.1","1.1")=-1
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+
+    }
+};
---
+++ leetcode_cn/cn/C++/question/762_Prime_Number_of_Set_Bits_in_Binary_Representation.cpp
@@ -0,0 +1,17 @@
+// @algorithm @lc id=767 lang=cpp 
+// @title prime-number-of-set-bits-in-binary-representation
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "algm/algm.h"
+using namespace std;
+// @test(6,10)=4
+// @test(10,15)=5
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        
+    }
+};
